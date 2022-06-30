ad2cf849e3b62a1079ab956aa73e07dd682b79d2
Francisco Calderón <fjcalderon@gmail.com>
2022-06-30 14:57:56 -0300

    Delete node_modules

--- node_modules/lightning/test/lnd_methods/fixtures/index.js
+++
@@ -1,6 +0,0 @@
-const getInfoResponse = require('./get_info_response');
-const getStatusResponse = require('./get_status_response');
-const lookupInvoiceResponse = require('./lookup_invoice_response');
-const queryRoutesResponse = require('./query_routes_response');
-
-module.exports = {getInfoResponse, lookupInvoiceResponse, queryRoutesResponse, getStatusResponse};
